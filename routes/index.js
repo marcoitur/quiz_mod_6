b6f1c143f4a71ec2bcb69d2c48b53adfc939031c
marcoitur <marcoitur2222@gmail.com>
2015-07-14 12:37:53 +0200

    Paso_9 solucion 3
    Sin Author

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
-var authorController = require("../controllers/author_controller");
 
-/* GET home page. "modificado" */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
-router.get('/author',authorController.author);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
